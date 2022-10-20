5ee0cdd48b32268e9ceda5c50823e2d7a0c0a615
kaisa <kaisa.eronen@aalto.fi>
2022-10-20 15:27:52 +0300

    get requests for persons and penalties work in localhost

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.get('/api/persons', (req, res) => {
 	})
 })
 
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => {
+			next(error)
+		})
+})
+
 app.get('/api/penalties', (req, res) => {
 	Penalty.find({}).then(penalties => {
 		res.json(penalties)
